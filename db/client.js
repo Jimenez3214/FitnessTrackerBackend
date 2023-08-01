327eb927e1a952673f4c17565825423eefbf418f
KierranyC <kierranyc@outlook.com>
2023-08-01 18:55:37 -0500

    still on migrations

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:kizpc@localhost:5433/fitness_tracker';
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:kizpc@localhost:5432/fitness_tracker';
 
 const client = new Pool({
   connectionString,
