6c779f73f5a4fbd009249bb4c4e2e7f54cf70d4b
KierranyC <kierranyc@outlook.com>
2023-08-01 18:47:13 -0500

    creating migrations

---
+++ migrations/1690931450188_my-first-migration.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('users', {
+    id: { type: 'serial', primaryKey: true },
+    username: { type: 'varchar(255)', notNull: true },
+    password: { type: 'varchar(255)', notNull: true }
+  })
+  pgm.createTable('activities', {
+    id: { type: 'serial', primaryKey: true },
+    name: { type: 'varchar(255)', notNull: true },
+    description: { type: 'text', notNull: true }
+  })
+  pgm.createTable('routines', {
+    id: { type: 'serial', primaryKey: true },
+    creatorId: { type: 'integer', references: '"users(id)"' },
+    isPublic: { type: 'boolean', default: false },
+    name: { type: 'varchar(255)', notNull: true },
+    goal: { type: 'text', notNull: true }
+  })
+  pgm.createTable('routine_activities', {
+    id: { type: 'serial', primaryKey: true },
+    routineId: { type: 'integer', references: '"routines(id)"' },
+    activityId: { type: 'integer', references: '"activities(id)"' },
+    duration: { type: 'integer' },
+    count: { type: 'integer' }
+  })
+  pgm.addConstraint('routine_activities',
+    '"routine_activities_unique_constraint"',
+    'UNIQUE ("routineId", "activityId")');
+};
+
+exports.down = pgm => {
+  pgm.dropTable('routine_activities')
+  pgm.dropTable('routines')
+  pgm.dropTable('activities')
+  pgm.dropTable('users')
+};
